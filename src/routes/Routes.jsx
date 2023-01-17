e981347da64b5ae0c2eab83b0bfffc510266b278
AlissonAlvarez <aligerald.cac7@gmail.com>
2023-01-16 23:35:33 -0500

    first commit project ecommerce react

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+
+function RoutesApp() {
+    return (
+        <Routes>
+            <Route>
+            </Route>
+        </Routes>
+    );
+}
+
+export default RoutesApp;
